5efc760b8746c521082811b2ba3f65264813eb7e
Krunal Dodiya <kunal.dodiya1@gmail.com>
2024-07-22 10:36:19 +0530

    done

---
+++ database/migrations/2024_07_22_045316_create_channels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("stream_id");
+            $table->string("channel_name");
+            $table->string("channel_number");
+            $table->string("channel_language")->nullable();
+            $table->string("channel_country")->nullable();
+            $table->enum("channel_quality", ["SD", "HD"])->default("SD");
+            $table->string("channel_logo")->nullable();
+            $table->string("channel_url");
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('channels');
+    }
+};
