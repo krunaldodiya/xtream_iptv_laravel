e9cce3e210c51656ed206cabbde6f382bde100a9
Krunal Dodiya <kunal.dodiya1@gmail.com>
2024-07-29 09:33:46 +0530

    done

---
+++ app/Models/StreamCategory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StreamCategory extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $dates = ['created_at', 'updated_at'];
+
+    public $timestamps = true;
+
+    public function streams() {
+        return $this->hasMany(Stream::class);
+    }
+}
